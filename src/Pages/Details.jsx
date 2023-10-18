2519327d408b627d230730dac769b4ff7a846cad
paulprince24 <97119638+paulprince24@users.noreply.github.com>
2023-10-18 14:33:14 +0530

    Update Details.jsx
    Token Added

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -97,6 +97,7 @@ export default function Details() {
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
+            localStorage.setItem("rftoken", token);
             // The signed-in user info.
             const user = result.user;
 
